5d24afd556682374150ef3a6b5697b3a0d893bcc
Dan Phillips <danmakenoise@gmail.com>
2016-04-13 14:53:27 -0400

    clarify room joining experience

--- frontend/components/menu.jsx
+++ frontend/components/menu.jsx
@@ -6,23 +6,46 @@ var Menu = React.createClass({
     router: React.PropTypes.object.isRequired
   },
 
+  getInitialState: function () {
+    return { roomCode: '' };
+  },
+
   render: function() {
     return(
       <main className='main menu'>
         <input
           ref='roomCode'
-          type='text' className='input' placeholder='Enter Room Code'></input>
-        <button className='button' onClick={this._joinGame}>
-          Submit Room Code
-        </button>
+          type='text'
+          value={this.state.roomCode}
+          className='input'
+          onChange={this._handleChange}
+          placeholder='Enter Room Code'>
+        </input>
+        {this._renderJoinButton()}
         <Link to='/game' className='link'>Start a New Game</Link>
       </main>
     );
   },
 
+  _renderJoinButton: function () {
+    if (this.state.roomCode.length === 4) {
+      return(
+        <button className='button' onClick={this._joinGame}>
+          Join Room!
+        </button>
+      );
+    }
+  },
+
+  _handleChange: function () {
+    if (this.refs.roomCode.value.length <= 4) {
+      this.setState({roomCode: this.refs.roomCode.value});
+    }
+  },
+
   _joinGame: function () {
     if(this.refs.roomCode.value.length === 4) {
-      var url = '/game/' + this.refs.roomCode.value;
+      var url = '/game/' + this.refs.roomCode.value.toLowerCase();
       this.context.router.push(url);
     }
   }

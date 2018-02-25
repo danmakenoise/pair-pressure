6bea721def5b49343ad7dd5b8e00896604b5b9ee
danmakenoise <danmakenoise@gmail.com>
2018-02-25 15:00:22 -0500

    server serves original assets

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (_, res) => {
-  res.send('Pair Pressure!');
-});
+app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Server is listening on localhost:3000'));

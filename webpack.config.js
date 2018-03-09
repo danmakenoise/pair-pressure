040f9d45282ac770e7d49ff9ac4aebe81e7e5692
danmakenoise <danmakenoise@gmail.com>
2018-03-09 08:35:31 -0500

    restore basic webpack functionality after upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,25 @@
-var path = require('path')
+const path = require('path')
 
-module.exports = {
-  context: __dirname,
+const config = {
   entry: './src/frontend/pair_pressure.js',
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/public')
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['react']
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
         }
-      },
-      {
-        test: /\.node$/,
-        loader: 'node-loader'
       }
     ]
-  },
-  devtool: 'eval-source-map'
+  }
 }
+
+module.exports = config

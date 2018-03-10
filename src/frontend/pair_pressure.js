e98f69588b56a3dfbe9e5907124e44f1930b736c
danmakenoise <danmakenoise@gmail.com>
2018-03-10 18:00:19 -0500

    fix(frontend): fix import statement at root

--- src/frontend/pair_pressure.js
+++ src/frontend/pair_pressure.js
@@ -1,7 +1,6 @@
+import App from './components/App'
 var React = require('react')
 var ReactDOM = require('react-dom')
-
-var App = require('./components/app')
 
 document.addEventListener('DOMContentLoaded', (event) => {
   var root = document.getElementById('pair-pressure')
